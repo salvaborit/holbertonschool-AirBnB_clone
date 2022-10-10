524cc73d5dbedc244e9525a8fdeaed81419d50bc
Salvador Borit <5084@holbertonschool.com>
2022-10-10 18:13:47 -0300

    create instance from dict

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,31 @@ import datetime
 class BaseModel:
     """Base class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Constructor"""
+        key_created_found = False
+        key_updated_found = False
+        datetime_format = '%Y-%d-%mT%H:%M:%S.%f'
+        if kwargs:
+            for key in kwargs:
+                if key == '__class__':
+                    continue
+                if key == 'created_at':
+                    self.created_at = datetime.datetime.strptime(
+                        kwargs[key], datetime_format)
+                    key_created_found = True
+                    continue
+                if key == 'updated_at':
+                    self.updated_at = datetime.datetime.strptime(
+                        kwargs[key], datetime_format)
+                    key_updated_found = True
+                    continue
+                self.key = kwargs[key]
         self.id = str(uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if key_created_found is False:
+            self.created_at = datetime.datetime.now()
+        if key_updated_found is False:
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """Prints string representation of class like this:
@@ -27,9 +47,9 @@ class BaseModel:
         """Returns a dictionary containing all
         keys/values of __dict__ of the instance"""
         instance_dict = self.__dict__
-        instance_dict['created_at'] = instance_dict.get(
-            'created_at').isoformat()
-        instance_dict['updated_at'] = instance_dict.get(
-            'updated_at').isoformat()
+        instance_dict['created_at'] = str(instance_dict.get(
+            'created_at').isoformat())
+        instance_dict['updated_at'] = str(instance_dict.get(
+            'updated_at').isoformat())
         instance_dict['__class__'] = type(self).__name__
         return instance_dict
