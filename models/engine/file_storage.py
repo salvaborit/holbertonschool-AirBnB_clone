01dd9706cdea3ae177d93125ea5fd9bd3195467b
Salvador Borit <salvaborit@gmail.com>
2022-10-12 16:23:53 -0300

    test reload/save/reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,7 +28,10 @@ class FileStorage:
         """Serializes '__objects' to the JSON file (path: __file_path)"""
         serializeable_objects = {}
         for key in self.__objects:
-            serializeable_objects[key] = self.__objects[key].to_dict()
+            if type(self.__objects[key]) is dict:
+                serializeable_objects[key] = self.__objects[key]
+            else:
+                serializeable_objects[key] = self.__objects[key].to_dict()
 
         with open(self.__file_path, 'w') as file:
             json.dump(serializeable_objects, file)
