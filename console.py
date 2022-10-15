0a4a61a82482fac017b21c14748e6248e058b7bc
Salvador Borit <salvaborit@gmail.com>
2022-10-15 20:54:42 -0300

    create show() method

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ import cmd
 import models
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -40,15 +41,36 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, arg):
         """Prints an instance's string representation
         based on class name and id"""
+        argv = arg.split()
+        if len(argv) == 0:
+            print('** class name missing **')
+        elif len(argv) == 1:
+            print('** instance id missing **')
+        else:
+            try:
+                eval(argv[0])
+                try:
+                    storage_dict = storage.all()
+                    instance = storage_dict[f'{argv[0]}.{argv[1]}']
+                    print(instance)
+                except:
+                    print('** id doesn\'t exist **')
+            except:
+                print('** class doesn\'t exist **')
 
     def do_destroy(self, arg):
-        """"""
+        """ """
 
     def do_all(self, arg):
-        """"""
+        """ """
 
     def do_update(self, arg):
-        """"""
+        """ """
+
+    def inst_validator(self, arg):
+        """Returns true if an ID corresponds to an existent
+        instance in 'storage.__objects' exists"""
+        arg
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
