bf01ada7fb19be38015d3bd423e0918e47d4312e
Salvador Borit <salvaborit@gmail.com>
2022-10-16 03:00:20 -0300

    change id types

--- models/review.py
+++ models/review.py
@@ -8,6 +8,6 @@ from models.base_model import BaseModel
 class Review(BaseModel):
     """class: Review (inherits from 'BaseModel')"""
 
-    place_id = 0 # it will be the Place.id
-    user_id = 0 # it will be the User.id
+    place_id = "" # it will be the Place.id
+    user_id = "" # it will be the User.id
     text = ""
