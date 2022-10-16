0aec39687b12f1cee08a3c138716e35dd0b2ddd7
Salvador Borit <salvaborit@gmail.com>
2022-10-16 03:20:34 -0300

    fix imports

--- console.py
+++ console.py
@@ -3,12 +3,16 @@
 
 
 import cmd
-from sys import argv
 import models
+from models import storage
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
-from models import storage
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
