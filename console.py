cd60c56d55ae5779ad6075b21f978d29a715782d
Salvador Borit <salvaborit@gmail.com>
2022-10-16 01:29:30 -0300

    updated update command

--- console.py
+++ console.py
@@ -113,32 +113,30 @@ class HBNBCommand(cmd.Cmd):
                 eval(argv[0])
             except:
                 print('** class doesn\'t exist **')
+                return
             inst_found = False
             for obj_id, obj in storage.all().items():
                 if obj_id == f'{argv[0]}.{argv[1]}':
                     setattr(obj, argv[2], argv[3])
                     inst_found = True
             if inst_found is False:
-                print('** class doesn\'t exist **')
+                print('** no instance found **')
 
-    # def inst_validator(self, inst_name, inst_id):
-    #     """Checks if an instance/id pair exists and logs in a dict"""
-    #     dict = {}
-    #     try:
-    #         eval(inst_name)
-    #         dict['class'] = True
-    #         try:
-    #             storage_dict = storage.all()
-    #             instance = storage_dict[f'{inst_name}.{inst_id}']
-    #             print(instance)
-    #             dict['id'] = True
-    #         except:
-    #             dict['id'] = False
-    #             print('** id doesn\'t exist **')
-    #     except:
-    #         dict['class'] = False
-    #         print('** class doesn\'t exist **')
-    #     return dict
+    def inst_validator(self, inst_name, inst_id):
+        """Checks if an instance/id pair exists and logs in a dict"""
+        dict = {}
+        try:
+            eval(inst_name)
+            dict['class'] = True
+            try:
+                storage_dict = storage.all()
+                instance = storage_dict[f'{inst_name}.{inst_id}']
+                dict['id'] = True
+            except:
+                dict['id'] = False
+        except:
+            dict['class'] = False
+        return dict
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
